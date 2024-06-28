9343c8ab6b1c55b9f0c1f55e7e814a5ec5642150
Domen Golob <domen.golob.px@gmail.com>
2024-06-28 11:22:36 +0200

    Implemented Pkce challange

--- OAuthTutorial/Code/Pkce.cs
+++ OAuthTutorial/Code/Pkce.cs
@@ -1,19 +1,40 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿global using CodeVerifier = string;
+global using CodeChallenge = string;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
 
 namespace OAuthTutorial.Code;
 
+/// <summary>
+/// Provides a randomly generated code verifier and challenge of PKCE (Proof Key for Code Exchange) implementation.
+/// </summary>
 public static class Pkce
 {
-    // https://datatracker.ietf.org/doc/html/rfc7636#section-4.1
-    public static string GenerateCodeVerifier(int length = 43)
+    /// <summary>
+    /// Generates a Code Verifier and a Code Challenge for PKCE (Proof Key for Code Exchange), as described in the RFC 7636
+    /// Code Verifier docs: https://datatracker.ietf.org/doc/html/rfc7636#section-4.1
+    /// Code Challenge docs: https://datatracker.ietf.org/doc/html/rfc7636#section-4.2
+    /// </summary>
+    /// <param name="size">Code verifier size in bytes, min 32 corresponds to a final length of 43 chars and max 96 to 128 chars.</param>
+    /// <returns>A tuple of two strings (CodeVerifier, CodeChallenge) </returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the size is less than 32 or greater than 96.</exception>
+    public static (CodeVerifier, CodeChallenge) GenerateCodeVerifierAndChallenge(int size = 32)
     {
-        if (length is < 43 or > 128)
+        if (size is < 32 or > 96)
         {
-            throw new ArgumentOutOfRangeException(nameof(length), "The length must be between 43 and 128 characters.");
+            throw new ArgumentOutOfRangeException(nameof(size), "The length must be between 43 and 128 characters.");
         }
 
-        var bytes = new byte[200];
+        Span<byte> bytes = stackalloc byte[size];
+        Span<char> codeVerifier = stackalloc char[(size + 2) / 3 * 4];
         Random.Shared.NextBytes(bytes);
-        return Base64UrlEncoder.Encode(bytes)[..length];
+        var written = Base64UrlEncoder.Encode(bytes, codeVerifier);
+
+        codeVerifier = codeVerifier[..written];
+
+        var challengeBytes = SHA256.HashData(bytes);
+        var challenge = Base64UrlEncoder.Encode(challengeBytes);
+
+        return (codeVerifier.ToString(), challenge);
     }
 }
---
+++ Tests/PkceTests.cs
@@ -0,0 +1,17 @@
+using OAuthTutorial.Code;
+using Xunit.Abstractions;
+
+namespace Tests;
+
+public class PkceTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void WorksForAllRanges()
+    {
+        for (var i = 32; i < 97; i++)
+        {
+            var (verifier, challenge) = Pkce.GenerateCodeVerifierAndChallenge(i);
+            testOutputHelper.WriteLine($"Bytes: {i:00}, Verifier length: {verifier.Length}, Verifier: {verifier}, Challenge: {challenge}");
+        }
+    }
+}
